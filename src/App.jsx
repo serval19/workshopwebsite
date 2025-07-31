ce6cba1987aefc25909020081baebdf41c313def
serval19 <diwatomy04@gmail.com>
2025-07-31 22:26:02 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ const App = () => {
     <div >
       <nav className='bg-blue-800 pt-4 pb-4 max-h-30 flex items-center flex-row justify-between'>
         <div><a href=''><img src={homepic} className='ml-5' alt="homebutton" /></a></div>
-        <div className='flex flex-row justify-end gap-20 mt-0'>
+        <div className='flex flex-row justify-end gap-10 mt-0'>
           
-          <a href="" className='text-white text-2xl'>About    </a>
-          <a href="" className='text-white text-2xl'>Works    </a>
-          <a href="" className='text-white text-2xl'>Location    </a>
-          <a href="" className='text-white text-2xl pr-10'>Contact    </a>
+          <a href="" className='transition-all duration-150 ease-in-out text-white text-2xl hover:bg-white hover:rounded-xl hover:text-black px-3 py-2'>About    </a>
+          <a href="" className='transition-all duration-150 ease-in-out text-white text-2xl hover:bg-white hover:rounded-xl hover:text-black px-3 py-2'>Works    </a>
+          <a href="" className='transition-all duration-150 ease-in-out text-white text-2xl hover:bg-white hover:rounded-xl hover:text-black px-3 py-2'>Location    </a>
+          <a href="" className='transition-all duration-150 ease-in-out text-white text-2xl mr-10 hover:bg-white hover:rounded-xl hover:text-black px-3 py-2'>Contact    </a>
         </div>
         
       </nav>
