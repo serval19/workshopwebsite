240af08ba780b04fa1aca1f9413e676e0c8323a7
serval19 <diwatomy04@gmail.com>
2025-07-31 22:13:00 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import homepic from './assets/homepic.jpg'
 
 const App = () => {
   return (
     <div >
-      <nav className='bg-blue-800 pt-4 pb-4'>
-        <div className='flex flex-row justify-end gap-20'>
+      <nav className='bg-blue-800 pt-4 pb-4 max-h-30 flex items-center flex-row justify-between'>
+        <div><a href=''><img src={homepic} className='ml-5' alt="homebutton" /></a></div>
+        <div className='flex flex-row justify-end gap-20 mt-0'>
           
           <a href="" className='text-white text-2xl'>About    </a>
           <a href="" className='text-white text-2xl'>Works    </a>
