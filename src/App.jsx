dd1959376db19ed14bf99338221190f60614922d
serval19 <diwatomy04@gmail.com>
2025-08-05 19:49:51 +0530

    added location

--- src/App.jsx
+++ src/App.jsx
@@ -70,8 +70,9 @@ const App = () => {
       className=" text-center text-blue-800 font-bold text-5xl">
         Location
       </motion.div>
-      <div>
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d601.9242766516262!2d76.78044754118584!3d9.59755394136179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b063575e79e1ed5%3A0xcd7b288cd2dab4a!2sDiWAN%20Automobiles!5e0!3m2!1sen!2sin!4v1754403030887!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div className='mt-10 flex justify-center'>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d601.9242766516262!2d76.78044754118584!3d9.59755394136179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b063575e79e1ed5%3A0xcd7b288cd2dab4a!2sDiWAN%20Automobiles!5e0!3m2!1sen!2sin!4v1754403030887!5m2!1sen!2sin" width="1000" height="550"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"
+         className='border-black border-2 rounded-2xl'></iframe>
 
       </div>
      
